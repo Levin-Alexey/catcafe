5b394cd39afe4e38e3379960a03bef4e4cd1ca4e
Levin-Alexey <levin.penza@yandex.ru>
2023-08-07 15:52:35 +0300

    Начальная стилизация

--- catcafe/src/components/app/App.js
+++ catcafe/src/components/app/App.js
@@ -1,14 +1,7 @@
-import logo from '../../logo.svg';
-import '../../App.css';
+import React from "react";
+import PageWrapper from "/src/components/layout/page-wrapper/page-wrapper";
+import "./style.css";
 
 export default function App() {
-  return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-      </div>
-  );
+    return <PageWrapper />;
 }
-
-
-export default App;
