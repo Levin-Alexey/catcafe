ce2bdb03e46766971b16b12b91320e06f94bbd32
Levin-Alexey <levin.penza@yandex.ru>
2023-08-07 12:48:01 +0300

    Добавил первичные файлы

--- catcafe/src/index.js
+++ catcafe/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from "/src/components/app/app";
+
+const rootElement = createRoot(document.getElementById("root"));
+rootElement.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
